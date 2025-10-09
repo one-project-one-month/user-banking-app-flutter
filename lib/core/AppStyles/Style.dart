aa5516bcf09376ae505bc875f5ed93f7f7a9eb57
PhoneWaiYanMoe <wyan40653@gmail.com>
2025-10-09 21:45:19 +0700

    based folders

---
+++ lib/core/AppStyles/Style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Style extends StatelessWidget {
+  const Style({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
